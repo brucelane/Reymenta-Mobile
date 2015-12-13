5aa9e63468244fbd69ce465bbd67455ea3c5b59c
Bruce Lane <bruce@batchass.fr>
2015-12-13 03:11:30 +0100

    start from MultiTouch sample

---
+++ androidstudio/ReymentaMobile/app/src/main/java/fr/batchass/reymentamobile/ReymentaMobileActivity.java
@@ -0,0 +1,7 @@
+package fr.batchass.reymentamobile;
+
+import org.libcinder.app.CinderNativeActivity;
+
+public class ReymentaMobileActivity extends CinderNativeActivity {
+    static final String TAG = "ReymentaMobileActivity";
+}
