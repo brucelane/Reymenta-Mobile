5824d910307945f01b29afa6af388513cfc26584
Bruce Lane <bruce@batchass.fr>
2015-12-13 09:52:05 +0100

    openFrameworks branch

--- androidstudio/ReymentaMobile/app/src/main/java/fr/batchass/reymentamobile/ReymentaMobileActivity.java
+++
@@ -1,7 +0,0 @@
-package fr.batchass.reymentamobile;
-
-import org.libcinder.app.CinderNativeActivity;
-
-public class ReymentaMobileActivity extends CinderNativeActivity {
-    static final String TAG = "ReymentaMobileActivity";
-}
